50f4823136ee36b56411ef429f30f80fe55ecc71
Ryszard Kubinski <ryszard.kubinski@mail.mcgill.ca>
2018-12-26 23:15:02 -0500

    figured out most variable bacteria

--- microbiome_read.py
+++ microbiome_read.py
@@ -26,24 +26,56 @@ def DF_create ():
 	temp =pd.DataFrame(data=seq["#OTU ID"])
 	temp.columns=["OTU"]
 	return temp
-def data_sep(newDF, inputcol):
+def df_prep(newDF, inputcol):
 	for i in inputcol:
 		newDF[i]=seq[i]	
+	newDF['AVG']=newDF.mean(axis=1)
 
-#we create new dataframes for healthy, UC and CD data
+def data_prep():
+	
+	df_prep(healthyData,healthy['dataID'])
+	df_prep(CDData,CD['dataID'])
+	df_prep(UCData,UC['dataID'])
+
+def dict_prep():
+	d={}
+	keys=healthyData['OTU']
+	for s in keys:
+		for j in healthyData[healthyData['OTU']==s].index:
+			d[s]=[healthyData['AVG'][j],CDData['AVG'][j],UCData['AVG'][j]] 
+	return d
+def var_dict(d):
+	variance={}
+	for k in dict.keys(d):
+		variance[k]=np.std(d[k])*np.std(d[k])
+	return variance
+def get_n_largest(dictionary, n):
+    return sorted([(value, key) for key, value in dictionary.items()],
+                  reverse=True)[:n]
+
+
+
+
+#we create new dataframes separated by the diagnosis of the patient
+
 healthyData = DF_create()
 CDData = DF_create()
 UCData = DF_create()
 
-#we separate our current data into these dataframes
-data_sep(healthyData,healthy['dataID'])
-data_sep(CDData,CD['dataID'])
-data_sep(UCData,UC['dataID'])
+data_prep()
+data_dict=dict_prep()
+variance=var_dict(data_dict)
+
+#we now have our most variant bacteria 
+most_variant=get_n_largest(variance,200)
+
+
+
 
 
 
 '''
-healthyData['AVG']=healthyData.mean(axis=1) 
+
 
 avgandName= df[['#OTU ID', "AVG"]]
 avgandName=avgandName[avgandName.AVG > 1]
